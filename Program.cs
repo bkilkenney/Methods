e52237f13622b649f88d6f6f9d420cd31b67ac11
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-22 23:24:56 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        //    Console.WriteLine("I'm going to count to ten");
+          //    CountToTen();  //"Calling" the method created below.  Main method is where all the action takes place
+          //    Console.WriteLine("I just counted to ten!");
+          //}
+
+        ////Creating a method that displays counting to 10  ^look up^
+        //static void CountToTen()  //This action/verb of counting to 10 is how you create a method
+        //{
+        //    for (int i = 1; i <= 10; i++) 
+        //    Console.WriteLine(i);
+
+
+        //Creating a method that "Returns" something
+
+           
+        }
+
+    }
+
+
+
